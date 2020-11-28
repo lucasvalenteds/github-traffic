0260ff31d2e4d03670b6b1031a3107268ff49702
Lucas <lucasvalenteds@gmail.com>
2020-11-28 19:58:41 -0300

    Adding source code

---
+++ traffic.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+personal_access_token="$1"
+if [ -z "$personal_access_token" ]
+then
+	printf "Error: personal access token not informed.\n"
+	printf "Usage: ./traffic.sh <personal_access_token>\n"
+	exit 1
+fi
+
+input_file="repositories.txt"
+[ ! -f "$input_file" ] && touch "$input_file"
+
+output_directory="data"
+[ ! -d "$output_directory" ] && mkdir "$output_directory"
+
+timestamp=$(date -u +"%Y-%m-%d %H_%M_%S")
+repositories=$(awk '$1=$1' FS=" " OFS="/" "$input_file")
+
+cd "$output_directory" || exit 1
+
+echo "$repositories" | tr ' ' '\n' | while read -r repository
+do
+	response=$(curl --silent \
+		--header "Authorization: token $personal_access_token" \
+		--header "Accept: application/vnd.github.v3+json" \
+		https://api.github.com/repos/"$repository"/traffic/views
+	)
+
+	directory=$(echo "$repository" | sed 's/\//_/')
+	[ ! -d "$directory" ] && mkdir "$directory"
+	echo "$response" > "$directory"/"$timestamp".json
+
+	printf "%s\n" "$repository"
+done
+
+cd ..
+
+exit 0
